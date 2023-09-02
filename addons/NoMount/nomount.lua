6509c073425d0e285d561e13dd3006f95b9062b4
ThornyFFXI <10786796+ThornyFFXI@users.noreply.github.com>
2023-09-02 10:04:44 -0400

    add regal chocobo song to nomount

--- addons/NoMount/nomount.lua
+++ addons/NoMount/nomount.lua
@@ -6,11 +6,13 @@ addon.link      = 'https://ashitaxi.com/';
 
 require('common');
 
+local blockedSongs = T{ 84, 212 };
+
 ashita.events.register('packet_in', 'packet_in_cb', function (e)
     -- Packet: Music Update
     if (e.id == 0x5F) then
         local song = struct.unpack('H', e.data, 0x06 + 1);
-        if (song == 84) then
+        if (blockedSongs:contains(song)) then
             e.blocked = true;
         end
     end
