e074a5870834ebb985ad0099ae7ff0eb6c198554
ThornyFFXI <10786796+ThornyFFXI@users.noreply.github.com>
2023-10-21 15:55:57 -0400

    1.01 - Add zonein packet to nomount

--- addons/NoMount/nomount.lua
+++ addons/NoMount/nomount.lua
@@ -1,6 +1,6 @@
 addon.name      = 'nomount';
 addon.author    = 'Thorny';
-addon.version   = '1.0';
+addon.version   = '1.01';
 addon.desc      = 'Blocks default mount music.';
 addon.link      = 'https://ashitaxi.com/';
 
@@ -9,6 +9,11 @@ require('common');
 local blockedSongs = T{ 84, 212 };
 
 ashita.events.register('packet_in', 'packet_in_cb', function (e)
+    -- Packet: Zone In
+    if (e.id == 0x0A) then
+        -- Zero active mount music
+        ashita.bits.pack_be(e.data_modified_raw, 0, 0x5E, 0, 16);
+    end
     -- Packet: Music Update
     if (e.id == 0x5F) then
         local song = struct.unpack('H', e.data, 0x06 + 1);
